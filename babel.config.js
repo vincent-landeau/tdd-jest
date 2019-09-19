9d202a99c532a8676dd7f453eb6f2bef3c9ff649
Jean-Baptiste Vilain <jbvilain@gmail.com>
2019-09-19 23:45:52 +0200

    Initial commit

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+  ],
+};
+
+// Todo : add katas (fizzbuzz etc...)
+// push project on Github
