e3f295fdaf2095c1c18c12ab9ad13eafdc164f99
Digital <plawinski.theo@gmail.com>
2021-10-15 11:07:31 +0200

    test Travis CI

--- src/sum.js
+++ src/sum.js
@@ -1,1 +1,9 @@
 // Implement the sum function here
+function sum(x, y) {
+    if (Number.isInteger(x + y)) {
+        return x + y;
+    } else {
+        return null;
+    }
+}
+module.exports = sum;
--- test/sum.test.js
+++ test/sum.test.js
@@ -1,1 +1,8 @@
 // Add the unit tests of the sum function here
+const sum = require('../src/sum.js');
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+});
+test('adds 1 + "2" return null', () => {
+    expect(sum(1, "2")).toBe(null);
+});
